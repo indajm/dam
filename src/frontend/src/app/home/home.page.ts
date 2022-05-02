3c19f2e58a2cdf27791e1e0f95ea078da1f19d2b
indajm <31355184+indajm@users.noreply.github.com>
2022-05-02 10:44:04 -0300

    frontend

---
+++ src/frontend/src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+//import { Dispositivo } from '../model/Dispositivo';
+import { DispositivoService } from '../services/dispositivo.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  //listadoDispositivo:Dispositivo[];
+
+  constructor(public dispositivoServ:DispositivoService) {}
+
+}
