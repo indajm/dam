ddf0daa9b7acdb6106964ad04c70fb290d48c42c
indajm <31355184+indajm@users.noreply.github.com>
2022-05-13 02:35:44 -0300

    Update index medicion

--- src/backend/routes/medicion/index.js
+++ src/backend/routes/medicion/index.js
@@ -3,8 +3,33 @@ var routerMedicion = express.Router();
 var pool = require('../../mysql');
 
 
-routerMedicion.get('/:id/mediciones', function(req, res) {
+routerMedicion.post('', function(req, res){
+    console.log("routerMedicion.post");
+    console.log(`req.body.fecha: ${req.body.fecha}`);
+    console.log(`req.body.valor: ${req.body.valor}`);
+    console.log(`req.body.dispositivoId: ${req.body.dispositivoId}`);
+    pool.query('INSERT INTO Mediciones (fecha, valor, dispositivoId) VALUES (?, ?, ?)', [req.body.fecha, req.body.valor, req.body.dispositivoId], function(err, result, fields){
+        if (err){
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+
+routerMedicion.get('/:id', function(req, res) {
     pool.query('SELECT * from Mediciones WHERE dispositivoId=?', [req.params.id], function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+routerMedicion.get('', function(req, res) {
+    pool.query('SELECT * from Mediciones', function(err, result, fields) {
         if (err) {
             res.send(err).status(400);
             return;
